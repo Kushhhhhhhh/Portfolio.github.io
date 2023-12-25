3a9e462633875ad9650658957b446d16c2e9e179
Kush Sharma <kush.73888@gmail.com>
2023-12-25 13:33:37 +0530

    added loading functionality

---
+++ client/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='w-full min-h-svh flex justify-center items-center'>
+      <span className="loading loading-spinner text-neutral"></span>
+    </div>
+  )
+}
+
+export default loading
